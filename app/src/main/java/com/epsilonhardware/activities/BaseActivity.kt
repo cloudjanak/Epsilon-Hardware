2b084c18f554039ab95d9d0c121a80cd7501da0a
Janak Thakkar <cloudjanak@gmail.com>
2023-04-06 11:34:34 +0530

    initial project commit

---
+++ app/src/main/java/com/epsilonhardware/activities/BaseActivity.kt
@@ -0,0 +1,42 @@
+package com.epsilonhardware.activities
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResult
+import androidx.appcompat.app.AppCompatActivity
+import com.epsilonhardware.core.BetterActivityResult
+import com.epsilonhardware.core.CustomProgressDialog
+
+/* Use this in Activities instead of startActivityForResult & requesting permissions
+Intent intent = new Intent(this, NewActivity.class);
+activityLauncher.launch(intent, result -> {
+    if (result.getResultCode() == Activity.RESULT_OK)
+
+});
+*/
+open class BaseActivity : AppCompatActivity(), View.OnClickListener {
+    var mActivity= Activity()
+    val Any.TAG: String
+        get() {
+            val tag = javaClass.simpleName
+            return if (tag.length <= 23) tag else tag.substring(0, 23)
+        }
+    lateinit var mDialog: CustomProgressDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mActivity = this
+        mDialog = CustomProgressDialog(this)
+    }
+
+    fun setUpClicks(vararg views: View) {
+        for (view in views) {
+            view.setOnClickListener(this)
+        }
+    }
+
+    override fun onClick(v: View) {}
+}
---
+++ app/src/main/java/com/epsilonhardware/activities/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.epsilonhardware.activities
+
+import android.app.Dialog
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.Window
+import android.webkit.*
+import com.epsilonhardware.core.isInternetAvailable
+import com.epsilonhardware.databinding.ActivityMainBinding
+import com.epsilonhardware.databinding.DialogInternetBinding
+
+
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (isInternetAvailable()) {
+            setWebView()
+        } else {
+            noInternetDialog()
+        }
+    }
+    private fun getUrl():String{
+        var baseUrl = "https://epsilonhardware.com/"
+        val launchUrl=intent.getStringExtra("url")
+        if (launchUrl!=null&& launchUrl.isNotEmpty()&&!launchUrl.equals("null")){
+            baseUrl=launchUrl
+        }
+        return baseUrl
+    }
+
+    private fun setWebView() {
+        binding.webView.loadUrl(getUrl())
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.settings.setSupportZoom(true)
+        binding.webView.settings.domStorageEnabled = true
+        binding.webView.settings.javaScriptCanOpenWindowsAutomatically = true
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?,
+                                                  request: WebResourceRequest?): Boolean {
+                val url = request?.url.toString()
+                view?.loadUrl(url)
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                if (!mActivity.isFinishing && !mDialog.isShowing) mDialog.show()
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                if (!mActivity.isFinishing && mDialog.isShowing) mDialog.dismiss()
+                if (!mActivity.isFinishing && binding.swipeToRefresh.isRefreshing) binding.swipeToRefresh.isRefreshing =
+                    false
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(view: WebView?,
+                                         request: WebResourceRequest?,
+                                         error: WebResourceError?) {
+                if (!mActivity.isFinishing && mDialog.isShowing) mDialog.dismiss()
+                if (!mActivity.isFinishing && binding.swipeToRefresh.isRefreshing) binding.swipeToRefresh.isRefreshing =
+                    false
+                val errorMessage = "Got Error! $error"
+                Log.e(TAG, error.toString())
+                //showToast(errorMessage)
+                super.onReceivedError(view, request, error)
+                if (error != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (error.errorCode == 400 || error.errorCode == 403) {
+                            clearCookies()
+                        }
+                    }
+                }
+            }
+
+            override fun onReceivedHttpError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                errorResponse: WebResourceResponse?
+            ) {
+                super.onReceivedHttpError(view, request, errorResponse)
+                Log.e(TAG, errorResponse.toString())
+                if (errorResponse != null) {
+                    if (errorResponse.statusCode == 400 || errorResponse.statusCode == 403) {
+                        clearCookies()
+                    }
+                }
+            }
+        }
+
+        binding.swipeToRefresh.setOnRefreshListener {
+            binding.swipeToRefresh.isRefreshing = false
+            binding.webView.reload()
+        }
+    }
+
+    private fun clearCookies() {
+        try {
+            CookieManager.getInstance().removeAllCookies(null)
+            CookieManager.getInstance().flush()
+        } catch (throwable:Throwable) {
+            throwable.printStackTrace()
+        }
+    }
+
+    private fun noInternetDialog() {
+        val dialog = Dialog(mActivity)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val internetDialogBinding = DialogInternetBinding.inflate(layoutInflater)
+        dialog.setContentView(internetDialogBinding.root)
+        dialog.setCancelable(false)
+
+        internetDialogBinding.cvAllow.setOnClickListener(View.OnClickListener {
+            mActivity.finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        })
+        dialog.show()
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            mActivity.finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
